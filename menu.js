36812d85e2e4a81dc1ce7ac0d71df60b730e54a5
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-06-21 12:58:05 +0530

    timeline and other changes

--- menu.js
+++ menu.js
@@ -1,15 +1,22 @@
-const menuOn = document.getElementById('menu-btn')
-const menuClose = document.getElementById('menu')
-let state = false; 
+const menuOn = document.getElementById("menu-btn");
+const menuClose = document.getElementById("menu");
+const menuClosing = document.getElementById("menuClose");
+let state = false;
 
-menuOn.addEventListener('click' ,()=>{
-    menuClose.style.transform = 'translateX(0px)'
-    menuClose.style.transition = "transform 700ms ease-in-out 0s"
-    console.log('helloworld')
-})
+menuOn.addEventListener("click", () => {
+  state = !state;
+  if (state) {
+    menuClose.style.transform = "translateX(0px)";
+    menuClose.style.display = "block";
+    menuClose.style.transition = "transform 700ms ease-in-out 0s";
+  }
+});
 
+menuClosing.addEventListener("click", () => {
+  state = !state;
+  if (!state) {
+    menuClose.style.transform = "translateX(100%)";
 
-menuClose.addEventListener('click',()=>{
-    menuClose.style.transform = 'translateX(100%)'
-    menuClose.style.transition = 'transform 700ms ease-in-out 0s'
-})
+    menuClose.style.transition = "transform 700ms ease-in-out 0s";
+  }
+});
