e7c8440c03a7e22e4d6370a1071c2e2df274a2d2
Gaurav Bhandari <87561983+kratos-respawned@users.noreply.github.com>
2023-06-22 09:50:02 +0530

    final build >> plssss

--- menu.js
+++ menu.js
@@ -1,22 +1,30 @@
 const menuOn = document.getElementById("menu-btn");
 const menuClose = document.getElementById("menu");
 const menuClosing = document.getElementById("menuClose");
+const mobileLinks = document.getElementsByClassName("close-menu");
 let state = false;
 
-menuOn.addEventListener("click", () => {
+const openMenu = () => {
   state = !state;
   if (state) {
     menuClose.style.transform = "translateX(0px)";
     menuClose.style.display = "block";
     menuClose.style.transition = "transform 700ms ease-in-out 0s";
   }
-});
+};
 
-menuClosing.addEventListener("click", () => {
+const closeMenu = () => {
   state = !state;
   if (!state) {
     menuClose.style.transform = "translateX(100%)";
 
     menuClose.style.transition = "transform 700ms ease-in-out 0s";
   }
-});
+};
+menuClosing.addEventListener("click", closeMenu);
+menuOn.addEventListener("click", openMenu);
+
+// close menu when mobile link is clicked
+for (let i = 0; i < mobileLinks.length; i++) {
+  mobileLinks[i].addEventListener("click", closeMenu);
+}
